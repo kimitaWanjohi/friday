e4ee2c2eab9efb30c4c63f20034e39d921c326f1
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-14 12:48:07 +0100

    added speech

--- friday/friday.py
+++ friday/friday.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import environs
+from speech import say
 
 env = environs.Env()
 
@@ -20,13 +21,32 @@ def completion_gpt3():
         model="gpt-3.5-turbo",
         messages=history
     )
-    return completion.choices[0]["message"].content
+    return completion.choices[0]["message"]
 
 def completion_gpt4():
     completion = openai.ChatCompletion.create(
         model="gpt-4",
         messages=history
     )
-    return completion.choices[0]["message"].content
+    return completion.choices[0]["message"]
 
 
+def chat():
+    say("Hello, my name is Friday. How can I help you today?")
+    while True:
+        prompt = input("You: ")
+        print()
+        if prompt == "exit":
+            break
+        history.append({
+            "role": "user",
+            "content": prompt
+        })
+        gpt_3 = completion_gpt3()        
+        # gpt_4 = completion_gpt4()
+        print()
+        say(gpt_3.content)
+        print("Friday:", gpt_3.content)
+        history.append(gpt_3)
+
+chat()
